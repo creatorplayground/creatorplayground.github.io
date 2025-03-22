2f1fd73a4219c612b9dc9f36a5f2938a1cc41603
smpsnxyz <lee@itsleesimpson.com>
2025-03-22 09:15:48 -0700

    Deploy site to GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/creatorplayground.github.io/",
+    build: {
+        outDir: ".", // write build output to root
+        emptyOutDir: false, // prevents deleting everything else
+    },
 });

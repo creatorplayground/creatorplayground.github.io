c185df6038af3274a806bddc06b31792191b4ac8
smpsnxyz <lee@itsleesimpson.com>
2025-04-15 16:25:09 -0700

    Updating config

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    base: "/",
     plugins: [react()],
-    build: {
-        outDir: "dist", // write build output to root
-        emptyOutDir: false, // prevents deleting everything else
-    },
 });
